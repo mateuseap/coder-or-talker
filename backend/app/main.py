6ca49a55429c80a822185d36c889204e54319841
Mateus Elias <mateuseap@mateuseap.com>
2025-02-02 14:30:50 -0300

    fix(backend/app/main.py): adjust CORS configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://www.mateuseap.com/coder-or-talker/"],
+    allow_origins=["https://www.mateuseap.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
