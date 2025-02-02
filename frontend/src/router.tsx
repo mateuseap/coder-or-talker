06ebf127177dea061c3d5f2f26fd046e80c65341
Mateus Elias <mateuseap@mateuseap.com>
2025-02-02 12:24:01 -0300

    fix(frontend): update router and vite config file to fix production problem
    Also, update homepage property of package.json file.

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -3,9 +3,12 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Results from "./pages/Results";
 
-const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/results", element: <Results /> },
-]);
+const router = createBrowserRouter(
+  [
+    { path: "/", element: <Home /> },
+    { path: "/results", element: <Results /> },
+  ],
+  { basename: "/coder-or-talker/" }
+);
 
 export default router;
