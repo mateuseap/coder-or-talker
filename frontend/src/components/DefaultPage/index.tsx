eeede3ac532dee36a252683deb4f9cc3c6eda4e1
Mateus Elias <mateuseap@mateuseap.com>
2025-02-02 10:05:14 -0300

    feat(frontend/src/components/DefaultPage): add Footer and update some styles

--- frontend/src/components/DefaultPage/index.tsx
+++ frontend/src/components/DefaultPage/index.tsx
@@ -1,9 +1,10 @@
 import type { DefaultPageProps } from "../../types";
+import Footer from "../Footer";
 import Navbar from "../Navbar";
 
 function DefaultPage({
-  className = "w-screen h-screen flex flex-col",
-  childrenClassName = "w-full h-full flex-1 gap-y-8 z-10",
+  className = "h-screen w-full relative isolate flex flex-col z-10 space-y-8",
+  childrenClassName = "w-full h-full flex-1",
   children = null,
   HtmlTag = "div",
 }: DefaultPageProps) {
@@ -11,6 +12,7 @@ function DefaultPage({
     <HtmlTag className={className}>
       <Navbar />
       {children && <main className={childrenClassName}>{children}</main>}
+      <Footer />
     </HtmlTag>
   );
 }
