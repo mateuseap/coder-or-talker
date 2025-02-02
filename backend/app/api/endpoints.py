045de1d148a63200f6b18c6c35485185a888db54
Mateus Elias <mateuseap@mateuseap.com>
2025-02-02 14:03:59 -0300

    feat(backend): add a fallback method to get GitHub data on rate limit exceed

--- backend/app/api/endpoints.py
+++ backend/app/api/endpoints.py
@@ -1,5 +1,5 @@
 import base64
-import requests
+import httpx
 from fastapi import APIRouter, HTTPException
 from starlette import status
 from ..services import github_service, bluesky_service
@@ -12,23 +12,36 @@ async def compare_github_and_bluesky_profiles(github: str, bluesky: str):
     try:
         github_profile_data = await github_service.fetch_user_profile(github)
         github_commits_count = await github_service.total_commits_fetcher(github)
-    except Exception:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Error fetching GitHub data.",
-        )
+    except Exception as e:
+        try:
+            github_data = await github_service.fetch_github_readme_stats_data(github)
+            github_profile_data["followers"] = github_data["followers"]
+            github_commits_count = github_data["total_commits"]
+        except Exception as fallback_error:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"Error fetching GitHub data: {str(e)}, and fallback also failed: {str(fallback_error)}",
+            )
 
     try:
         bluesky_profile_data = await bluesky_service.fetch_user_profile(bluesky)
-    except Exception:
+    except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Error fetching Bluesky data.",
+            detail=f"Error fetching Bluesky data: {str(e)}",
         )
 
-    response = requests.get(bluesky_profile_data["avatar"])
-    avatar_image_bytes = response.content
-    avatar_image_base64 = base64.b64encode(avatar_image_bytes).decode("utf-8")
+    try:
+        async with httpx.AsyncClient() as client:
+            avatar_response = await client.get(bluesky_profile_data["avatar"])
+            avatar_response.raise_for_status()
+            avatar_image_bytes = avatar_response.content
+            avatar_image_base64 = base64.b64encode(avatar_image_bytes).decode("utf-8")
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Error fetching Bluesky avatar: {str(e)}",
+        )
 
     return {
         "name": bluesky_profile_data["displayName"],
--- backend/app/services/github_service.py
+++ backend/app/services/github_service.py
@@ -1,4 +1,6 @@
 import httpx
+import re
+from fastapi import HTTPException, status
 
 GITHUB_API_URL = "https://api.github.com"
 
@@ -20,3 +22,36 @@ async def total_commits_fetcher(username: str) -> int:
         data = response.json()
         total_count = data.get("total_count")
         return total_count
+
+
+async def fetch_github_readme_stats_data(username: str) -> dict:
+    github_stats_url = f"https://github-readme-stats.vercel.app/api?username={username}&include_all_commits=true"
+    followers_url = f"{GITHUB_API_URL}/users/{username}/followers"
+
+    async with httpx.AsyncClient() as client:
+        try:
+            stats_response = await client.get(github_stats_url)
+            stats_response.raise_for_status()
+
+            commits_pattern = r"Total Commits\s*:\s*(\d+)"
+            commits_match = re.search(commits_pattern, stats_response.text)
+            if commits_match:
+                total_commits = int(commits_match.group(1))
+            else:
+                raise HTTPException(
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail="Could not extract total commits.",
+                )
+
+            followers_response = await client.get(followers_url)
+            followers_response.raise_for_status()
+            followers_data = followers_response.json()
+            total_followers = len(followers_data)
+
+            return {"commits_count": total_commits, "followers": total_followers}
+
+        except httpx.RequestError as e:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"Error fetching GitHub data: {str(e)}",
+            )
